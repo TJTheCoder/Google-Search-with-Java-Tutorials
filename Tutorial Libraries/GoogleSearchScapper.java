e1a4057a2408959fccc29567a0bd4c834e70be23
GOXR3PLUS <crAlexcomp@gmail.com>
2017-06-11 09:07:15 +0300

    Created the repository with code

---
+++ Tutorial Libraries/GoogleSearchScapper.java
@@ -0,0 +1,45 @@
+
+import java.io.IOException;
+import java.net.URLEncoder;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+/**
+ * [[SuppressWarningsSpartan]]
+ */
+public class GoogleSearchScapper {
+	
+	public static void main(String[] args) {
+		
+		String encoding = "UTF-8";
+		
+		try {
+			String searchText = "donald trump wikipedia";
+			Document duckduckgo = Jsoup.connect("https://duckduckgo.com/?q=" + URLEncoder.encode(searchText, encoding)).userAgent("Mozilla/5.0").get();
+			
+			Elements webSitesLinks = duckduckgo.getElementsByClass("result__url__domain");
+			
+			//Check if any results found
+			if (webSitesLinks.isEmpty()) {
+				System.out.println("No results found");
+				return;
+			}
+			
+			webSitesLinks.forEach(link->System.out.println(link.text()));
+			
+			//========If you want to get the first link ================
+			//String firstWebSiteLink = webSitesLinks.get(0).text();
+			
+			//System.out.println(firstWebSiteLink);
+			
+			//Move to the next page code..
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
